5c695d1c0484b2fb838b469925a5232090e73571
intlabs <petebirley@gmail.com>
2014-06-04 03:57:33 +0100

    mount dropbox

--- startup.sh
+++ startup.sh
@@ -33,3 +33,6 @@ echo "run the follwing command to then mount your dropbox account:"
 echo "mkdir -p ~/Dropbox && /ff4d/ff4d.py -at <YOURACCESSKEY> -ar ~/Dropbox &"
 
 /ff4d/getDropboxAccessToken.py -ak $app_key -as $app_secret -c $authorization_code
+mkdir -p ~/Dropbox
+
+/ff4d/ff4d.py -ar ~/Dropbox &
