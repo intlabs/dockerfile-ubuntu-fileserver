7d7da24cdff19eb8f5ce0a9c8fe9d631ad21af5b
intlabs <petebirley@gmail.com>
2014-06-04 04:36:16 +0100

    update startup script

--- startup.sh
+++ startup.sh
@@ -1,18 +1,54 @@
 #!/bin/sh
 # (c) Pete Birley
 
-apt-get install -y fuse
-
 #Startup ssh
 /usr/sbin/sshd -D &
 
 set -e
 
-
 app_key="${1}"
 app_secret="${2}"
 authorization_code="${3}"
 
+echo "********************************************************************************"
+echo "*                                                                              *"
+echo "*    create a new dropbox api app and use its generated output                 *"
+echo "*    go to the url specified bellow  (eaxple for cannyos testing)              *"
+echo "*                                                                              *"
+echo "********************************************************************************"
+echo ""
+echo "https://www.dropbox.com/1/oauth2/authorize?response_type=code&client_id=axq0c8se5oo7ndh"
+echo ""
+echo "********************************************************************************"
+echo "*                                                                              *"
+echo "*    this docker containter should then be launched using the command          *"
+echo "*                                                                              *"
+echo "********************************************************************************"
+echo ""
+echo "sudo docker run -it --rm -p 222:22 -p 111:111 -p 2049:2049 --privileged=true --lxc-conf=\"native.cgroup.devices.allow = c 10:229 rwm\" intlabs/dockerfile-ubuntu-fileserver <app_key> <app_secret> <authorization_code>"
+echo ""
+echo "********************************************************************************"
+echo "*                                                                              *"
+echo "*    Your dropbox drive will be mounted at ~/dropbox by default                *"
+echo "*                                                                              *"
+echo "*                                                                              *"
+echo "*               (c) Pete Birley 2014 - petebirley@gmail.com                    *"
+echo "*                                                                              *"
+echo "********************************************************************************"
+echo ""
+
+
+#Install fuse - this is a really ugly hack to deal with fuse in dropbox during development
+apt-get install -y fuse
+
+#Get access token
+/ff4d/getDropboxAccessToken.py -ak $app_key -as $app_secret -c $authorization_code &
+
+#Make mountpoint
+mkdir -p ~/dropbox
+
+#Launch Dropbox FUSE
+/ff4d/ff4d.py -ar -bg ~/dropbox 
 
 # Define exports
 echo '/home   *(rw,sync,fsid=0,no_subtree_check)' >> /etc/exports
@@ -20,19 +56,12 @@ echo '/home   *(rw,sync,fsid=0,no_subtree_check)' >> /etc/exports
 #Startup NFS server
 runsvdir /etc/sv &
 
-echo "to run the official dropbox util and mount the sync to ~/Dropbox:"
-echo "/dropbox-dist/dropboxd"
-echo " "
-echo "to run the fuse dropbox filespace util and mount the sync to ~/Dropbox:"
-echo "/ff4d/getDropboxAccessToken.py"
-echo "goto https://www.dropbox.com/developers/apps"
-echo "create a new dropbox api app and use its generated output, go to the url specified"
-echo "enter the responce and record the access token"
-echo "https://www.dropbox.com/1/oauth2/authorize?response_type=code&client_id=axq0c8se5oo7ndh"
-echo "run the follwing command to then mount your dropbox account:"
-echo "mkdir -p ~/Dropbox && /ff4d/ff4d.py -at <YOURACCESSKEY> -ar ~/Dropbox &"
 
-/ff4d/getDropboxAccessToken.py -ak $app_key -as $app_secret -c $authorization_code
-mkdir -p ~/Dropbox
-/ff4d/ff4d.py -ar -bg ~/Dropbox 
+#Announce bonus package option - can be used to sync folders.
+echo "BONUS!"
+echo "to run the official dropbox util and mount & sync to ~/Dropbox:"
+echo "/dropbox-dist/dropboxd"
+
+# Drop into a command prompt
 bash
+
